dc0f79fb180ddaaafa12d3ce5ed702e28024ac34
Kenneth Blix <kennethblix@gmail.com>
2017-12-04 14:02:35 +0100

    Layout for Server

---
+++ ServerClient/ServerClient/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace ServerClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BtnStart = new System.Windows.Forms.Button();
+            this.TxtHost = new System.Windows.Forms.TextBox();
+            this.TxtPort = new System.Windows.Forms.TextBox();
+            this.BtnStop = new System.Windows.Forms.Button();
+            this.TxtStatus = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(40, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Host:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(409, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 42);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Port:";
+            // 
+            // BtnStart
+            // 
+            this.BtnStart.Location = new System.Drawing.Point(826, 24);
+            this.BtnStart.Name = "BtnStart";
+            this.BtnStart.Size = new System.Drawing.Size(247, 49);
+            this.BtnStart.TabIndex = 1;
+            this.BtnStart.Text = "Start";
+            this.BtnStart.UseVisualStyleBackColor = true;
+            // 
+            // TxtHost
+            // 
+            this.TxtHost.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtHost.Location = new System.Drawing.Point(151, 24);
+            this.TxtHost.Name = "TxtHost";
+            this.TxtHost.Size = new System.Drawing.Size(247, 49);
+            this.TxtHost.TabIndex = 2;
+            // 
+            // TxtPort
+            // 
+            this.TxtPort.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtPort.Location = new System.Drawing.Point(520, 24);
+            this.TxtPort.Name = "TxtPort";
+            this.TxtPort.Size = new System.Drawing.Size(247, 49);
+            this.TxtPort.TabIndex = 2;
+            // 
+            // BtnStop
+            // 
+            this.BtnStop.Location = new System.Drawing.Point(1095, 24);
+            this.BtnStop.Name = "BtnStop";
+            this.BtnStop.Size = new System.Drawing.Size(247, 49);
+            this.BtnStop.TabIndex = 1;
+            this.BtnStop.Text = "Stop";
+            this.BtnStop.UseVisualStyleBackColor = true;
+            // 
+            // TxtStatus
+            // 
+            this.TxtStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtStatus.Location = new System.Drawing.Point(151, 103);
+            this.TxtStatus.Multiline = true;
+            this.TxtStatus.Name = "TxtStatus";
+            this.TxtStatus.Size = new System.Drawing.Size(1191, 319);
+            this.TxtStatus.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1440, 487);
+            this.Controls.Add(this.TxtStatus);
+            this.Controls.Add(this.TxtPort);
+            this.Controls.Add(this.TxtHost);
+            this.Controls.Add(this.BtnStop);
+            this.Controls.Add(this.BtnStart);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Server";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button BtnStart;
+        private System.Windows.Forms.TextBox TxtHost;
+        private System.Windows.Forms.TextBox TxtPort;
+        private System.Windows.Forms.Button BtnStop;
+        private System.Windows.Forms.TextBox TxtStatus;
+    }
+}
+
