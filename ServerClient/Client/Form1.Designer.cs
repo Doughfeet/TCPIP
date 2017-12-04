d32c612c7049b47ff48e42f6f26b743490513693
Kenneth Blix <kennethblix@gmail.com>
2017-12-04 14:09:45 +0100

    Client

---
+++ ServerClient/Client/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtPort = new System.Windows.Forms.TextBox();
+            this.TxtHost = new System.Windows.Forms.TextBox();
+            this.BtnSend = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtMessage = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.TxtStatus = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // TxtPort
+            // 
+            this.TxtPort.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtPort.Location = new System.Drawing.Point(515, 29);
+            this.TxtPort.Name = "TxtPort";
+            this.TxtPort.Size = new System.Drawing.Size(247, 49);
+            this.TxtPort.TabIndex = 6;
+            // 
+            // TxtHost
+            // 
+            this.TxtHost.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtHost.Location = new System.Drawing.Point(146, 29);
+            this.TxtHost.Name = "TxtHost";
+            this.TxtHost.Size = new System.Drawing.Size(247, 49);
+            this.TxtHost.TabIndex = 7;
+            // 
+            // BtnSend
+            // 
+            this.BtnSend.Location = new System.Drawing.Point(821, 454);
+            this.BtnSend.Name = "BtnSend";
+            this.BtnSend.Size = new System.Drawing.Size(247, 49);
+            this.BtnSend.TabIndex = 5;
+            this.BtnSend.Text = "Send";
+            this.BtnSend.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(404, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 42);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Port:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(35, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 42);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Host:";
+            // 
+            // TxtMessage
+            // 
+            this.TxtMessage.Location = new System.Drawing.Point(146, 110);
+            this.TxtMessage.Multiline = true;
+            this.TxtMessage.Name = "TxtMessage";
+            this.TxtMessage.Size = new System.Drawing.Size(922, 306);
+            this.TxtMessage.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(821, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(247, 49);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // TxtStatus
+            // 
+            this.TxtStatus.Location = new System.Drawing.Point(146, 536);
+            this.TxtStatus.Multiline = true;
+            this.TxtStatus.Name = "TxtStatus";
+            this.TxtStatus.Size = new System.Drawing.Size(922, 350);
+            this.TxtStatus.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1319, 957);
+            this.Controls.Add(this.TxtStatus);
+            this.Controls.Add(this.TxtMessage);
+            this.Controls.Add(this.TxtPort);
+            this.Controls.Add(this.TxtHost);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.BtnSend);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtPort;
+        private System.Windows.Forms.TextBox TxtHost;
+        private System.Windows.Forms.Button BtnSend;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtMessage;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox TxtStatus;
+    }
+}
+
